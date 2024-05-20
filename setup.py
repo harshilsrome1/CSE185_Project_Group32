ebd8ab13a65d60787c5c17a7be8d66b46d86b7bd
harshilsrome1 <136511470+harshilsrome1@users.noreply.github.com>
2024-05-20 09:17:33 -0700

    Update and rename gibbs_sampler/setup.py to setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'spyglass/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+
+
+setup(
+    name='spyglass',
+    version=VERSION,
+    description='CSE 185 Final Project',
+    author='Michael Chan, Kyra Fetter, Jessica Wang',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "spyglass=spyglass.spyglass:main"
+        ],
+    },
+)
