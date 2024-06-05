219bc103862b05d2fbdad501c30d2243c7062056
harshilsrome1 <136511470+harshilsrome1@users.noreply.github.com>
2024-06-04 21:31:36 -0700

    Update gibbs_algorithm.py

--- gibbs_sampler/gibbs_algorithm.py
+++ gibbs_sampler/gibbs_algorithm.py
@@ -1,12 +1,15 @@
 import random 
 from tqdm import tqdm
 
+#this is the main gibbs sampler algorithm, it takes in 4 inputs and returns a list with the set of motifs we are looking for
 def gibbs_sampler(dna: list[str], k: int, t: int, n: int) -> list[str]:
-    bestAnswers = []
+    bestAnswers = [] #this stores the best set of motifs to return 
+    #this for loop creates the first set of random motifs 
     for eachdna in dna:
         number = rand(len(eachdna)-k)
         first = eachdna[number:number+k]
         bestAnswers.append(first)
+    #this for loop iterates 1000 times, replacing a random motif each time and checking if score improved 
     for j in tqdm(range(1000)):
         motifs = []
         bestMotifs = []
@@ -38,12 +41,16 @@ def gibbs_sampler(dna: list[str], k: int, t: int, n: int) -> list[str]:
             randomIndex = randIndex[0]
             kmer = text[randomIndex:randomIndex+k]
             motifs[numberi] = kmer
+            #replace set of motifs if score improved when we repeat for range of n
             if(score(motifs) < score(bestMotifs)):
                 bestMotifs = motifs
+        #replace set of motifs if score improved at end of each of the 1000 ierations within the for loop 
         if(score(bestMotifs) < score(bestAnswers)):
             bestAnswers = bestMotifs;
     return(bestAnswers)
 
+#function takes in a set of sequences and produces a matrix of rows based on the length of the sequences and 4 columns, each corresponding to the 4 nucleotides: A, C, G, and T 
+#each column is a probability distribution of the likelihoods of each nucleotide appearing in that spot based on the motif matrix we are given.
 def profile(motifs) -> list[dict[str, float]]:
     answer = []
     size = len(motifs)
@@ -67,15 +74,17 @@ def profile(motifs) -> list[dict[str, float]]:
             if(motifs[d][q] == "T"):
                 matrix[q]["T"] += 1/sized
     return matrix
-    
+
+#produces a random integer between 0 and n
 def rand(n: int) -> int: 
     return(random.randint(0, n-1))
 
+#takes in an input of a set of motifs and produces a score for that motif set, and we want to minimize the score
+#score is assigned to each column based on the number of nucleotides in that column minus the most frequent nucleotide showing up in that column.
 def score(motifs):
     score = 0
     size = len(motifs[0])
     num = len(motifs)
-    
     for m in range(size):
         a = 0
         c = 0
@@ -93,6 +102,8 @@ def score(motifs):
         score += (len(motifs)-max(a,c,g,t))
     return score
 
+#Takes inputs of a (long) sequence, an integer k for k-mer size we want, and the profile of the motif set
+#the code uses these inputs to go through all possible k-mers from the sequence, and calculate which is the most probable k-mer based on the profile matrix given
 def profile_most_probable_kmer(text: str, k: int, profile: list[dict[str, float]]) -> str:
     n = len(text)
     percent = 1
@@ -105,6 +116,8 @@ def profile_most_probable_kmer(text: str, k: int, profile: list[dict[str, float]
             answer = pattern
     return answer
 
+#Takes inputs of a string called pattern and the profile matrix based on a set of motifs and uses the probability distribution of each column to calculate the total probability of pattern occurring
+#The function returns this probability and the function is used to find the profile most probable kmer.
 def stringProb(pattern: str, profile: list[dict[str, float]]):
     percent = 1
     value = 0
